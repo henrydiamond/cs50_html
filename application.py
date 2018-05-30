8efe9232a6c3b2e4ec340866defeaaae503d52e4
drjquang <daoduyquang.eng@gmail.com>
2018-05-30 15:26:28 +0700

    Building a server to serve table.html

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Nothing here"
+
+@app.route("/table")
+def table():
+    return render_template("table.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
