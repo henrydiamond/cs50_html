c8f65699996986f3f335594e33d572d8c4257ed5
henrydiamond <henrydiamond.tech@gmail.com>
2018-05-31 18:31:59 +0700

    Using flexbox to fit the screen

--- application.py
+++ application.py
@@ -17,5 +17,10 @@ def printmedia():
 def viewport():
     return render_template("viewport.html")
 
+@app.route("/flexbox")
+def flexbox():
+    return render_template("flexbox.html")
+	
+# Run server with python
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
